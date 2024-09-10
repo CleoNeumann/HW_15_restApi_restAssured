5db0011963740322b808fb92287c3a1326f64423
Anastasiya Bessmertnova <avbessmertnova@gmail.com>
2024-09-10 23:00:13 +0400

    add api tests add build.gradle

---
+++ src/test/java/tests/RegistrationTests.java
@@ -0,0 +1,71 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class RegistrationTests extends TestBase {
+
+    @Test
+    void successfulRegistrationTest() {
+        String data = "{\"email\": \"eve.holt@reqres.in\"," +
+                " \"password\": \"pistol\"}";
+        String token = given()
+                .body(data)
+                .contentType(JSON)
+                .log().body()
+
+                .when()
+                .post("/register")
+
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("id", is(4))
+                .body("token", notNullValue())
+                .extract().path("token");
+        assertThat(token)
+                .hasSizeGreaterThan(10)
+                .isAlphanumeric();
+    }
+
+    @Test
+    void registrationWithNoPasswordTest() {
+        String data = "{\"email\": \"eve.holt@reqres.in\"}";
+        given()
+                .body(data)
+                .contentType(JSON)
+                .log().body()
+
+                .when()
+                .post("/register")
+
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+    }
+
+    @Test
+    void registrationWithNoEmailTest() {
+        String data = "{\"password\": \"pistol\"}";
+        given()
+                .body(data)
+                .contentType(JSON)
+                .log().body()
+
+                .when()
+                .post("/register")
+
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing email or username"));
+    }
+}
---
+++ src/test/java/tests/UserListTests.java
@@ -0,0 +1,58 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.*;
+
+public class UserListTests extends TestBase{
+
+    @Test
+    void getUsersListTest() {
+        given()
+                .when()
+                .get("/users")
+
+                .then()
+                .log().body()
+                .statusCode(200)
+                .body("total", is(12))
+                .body("data[4].first_name", is("Charles"))
+                .body("data[4].last_name", is("Morris"));
+    }
+
+    @Test
+    void checkSelectedUserEmailTest() {
+        given()
+                .when()
+                .get("/users/5")
+
+                .then()
+                .log().body()
+                .statusCode(200)
+                .body("data.email", is("charles.morris@reqres.in"));
+    }
+
+    @Test
+    public void createNewUserTest() {
+        String data = "{\"name\": \"Cleo\", \"job\": \"qa\"}";
+        given()
+                .body(data)
+                .contentType(JSON)
+                .log().body()
+
+                .when()
+                .post("/users")
+
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(201)
+                .body("name", is("Cleo"))
+                .body("job", is("qa"))
+                .body("createdAt", startsWith(String.valueOf(LocalDate.now())));
+    }
+}
